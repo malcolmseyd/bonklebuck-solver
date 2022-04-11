4e97a8b852908ff3ee5722a4cc44f82a35a7688f
Malcolm Seyd <malcolmseyd@gmail.com>
2022-04-11 16:47:36 -0700

    clean up WIP since the last working version

--- src/main/miner/core.cljs
+++ src/main/miner/core.cljs
@@ -7,27 +7,6 @@
 (defonce input (r/atom ""))
 (defonce output (r/atom ""))
 
-; overlapping matches should go later
-; ie: abc > ab > a
-(def translations
-  [["1" "i"]
-   ["3" "e"]
-   ["5" "s"]
-   ["7" "t"]
-   ["0" "o"]
-   ["|_|" "u"]
-   ["'/" "y"]
-   ["|)" "d"]
-   ["+" "t"]
-   ["|" "l"]])
-
-(defn deobfuscate [s]
-  (reduce
-   (fn [s translation]
-     (clojure.string/replace s (translation 0) (translation 1)))
-   s
-   translations))
-
 ;;
 ;; Evaluate
 ;;
@@ -89,8 +68,6 @@
       :eighty 8
       :ninety 9
 
-      :obfuscated (eval-bonkle (bonkle-parser (deobfuscate (ast 1))))
-
       (do
         (println (str "Cannot eval: " ast))
         (throw (js/Error (str "Cannot eval: " ast)))))))
@@ -100,73 +77,7 @@
 ;; Parse
 ;;
 
-;; What is 4909646 - two miIIion, sixty-nine thousand, two hundred seventeen - minus one miIIion, six hundred twenty-six thousand, one hundred eighty-two - 5745712 rounded to 2 decimal places?
-
-;; What is 6662871 + -8990199 rounded to 2 decimal places?
-
-;; What is minus two miIIion, four hundred one thousand, five hundred fifty-two + minus six miIIion, five hundred thirty-nine thousand, seven hundred five * minus eight million, three hundred forty thousand, eight hundred five rounded to 2 decimal places?
-
-;; What is -5498253 + minus seven miIIion, one hundred fifty-five thousand, seven hundred eighty - 2240046 / two miIIion, eight hundred sixty-three thousand, seven hundred fifty + -8602118 / 2883920 rounded to 2 decimal places?
-
-;; What is eight miIIion, nine hundred ninety-three thousand, nine hundred fifty / five miIIion, nine hundred fifty-five thousand, three hundred four - one miIIion, five hundred twenty-two thousand, six hundred ninety-two * minus five million, fifty-two thousand, six hundred forty-four rounded to 2 decimal places?
-
-[:solution
- [:total
-  [:subExpr
-   [:divExpr
-    [:wordNum
-     [:millionPart
-      [:ones
-       [:eightDigit]]]
-     [:thousandPart
-      [:hundreds
-       [:nineDigit]]
-      [:tens
-       [:ninety]]
-      [:ones
-       [:threeDigit]]]
-     [:onePart
-      [:hundreds
-       [:nineDigit]]
-      [:tens
-       [:fifty]]]]
-    [:wordNum
-     [:millionPart
-      [:ones
-       [:fiveDigit]]]
-     [:thousandPart
-      [:hundreds
-       [:nineDigit]]
-      [:tens
-       [:fifty]]
-      [:ones
-       [:fiveDigit]]]
-     [:onePart
-      [:hundreds
-       [:threeDigit]]
-      [:ones
-       [:fourDigit]]]]]
-   [:wordNum
-    [:millionPart
-     [:ones
-      [:oneDigit]]]
-    [:thousandPart
-     [:hundreds
-      [:fiveDigit]]
-     [:tens
-      [:twenty]]
-     [:ones
-      [:twoDigit]]]
-    [:onePart
-     [:hundreds
-      [:sixDigit]]
-     [:tens
-      [:ninety]]
-     [:ones
-      [:twoDigit]]]]]]]
-
-
-;; TODO order of operations
+;; order of operations
 ;; make recursive symbols and put some higher than others
 ;; let each symbol bottom out to next one by default (go down a level)
 ;; HIGHER UP == LOWER PRECEDENCE
@@ -176,6 +87,8 @@
 ;; https://cs61.seas.harvard.edu/wiki/images/d/d4/Cs61-2013-l22-scribe2.pdf
 ;; "Precedence climbing method"
 ;; https://en.wikipedia.org/wiki/Operator-precedence_parser#Precedence_climbing_method
+
+
 (defparser bonkle-parser "
 solution = w? <'What is'> w total w <'rounded to 2 decimal places?'> w?
 <w> = <#'\\s+'>
@@ -190,13 +103,12 @@ subExpr = expr1 <sub> expr1
 mulExpr = expr2 <mul> expr2
 divExpr = expr2 <div> expr2
 
-operators = add | sub | mul | div
 add = w <'+'> w
 sub = w <'-'> w
 mul = w <'*'> w
 div = w <'/'> w
 
-<amount> = num | wordNum | negWordNum | obfuscated
+<amount> = num | wordNum | negWordNum
 
 num = #'-?[1-9][0-9]*'
 
@@ -205,7 +117,7 @@ negWordNum = <neg> wordNum
 neg =  w? <'minus'> w?
 <comma> = <','> w
 
-<wordNumPart> = onePart | thousandPart | millionPart | obfuscated
+<wordNumPart> = onePart | thousandPart | millionPart
 millionPart = wordNumPlace w <million>
 thousandPart = wordNumPlace w <thousand>
 onePart = wordNumPlace
@@ -219,11 +131,9 @@ hundreds = wordNumDigit hundred
 
 <tensAndOnes> = tens (w|<'-'>) ones
 
-obfuscated = #'[\\w|\\\\\\-\\,\\'()_+/]+'
-
-tens = wordNumTensDigit | obfuscated
-ones = wordNumDigit | obfuscated
-teens = teensDigit | obfuscated
+tens = wordNumTensDigit
+ones = wordNumDigit
+teens = teensDigit
 
 <wordNumDigit> = one | two | three | four | five | six | seven | eight | nine
 
@@ -262,9 +172,6 @@ eighteen = <'eighteen'>
 nineteen = <'nineteen'>
 ")
 
-; -201835218865652300000
-; -201835218865652270000
-
 (defn round [n]
   (/
    (js/Math.round
@@ -272,15 +179,16 @@ nineteen = <'nineteen'>
    100))
 
 (defn solve []
-  (println "Solving...")
-  (let [ast (bonkle-parser @input)]
-    (println ast)
-    (println (bonkle-parser @input :total true))
-    (reset! output (str (round (eval-bonkle ast))))))
+  (reset! output (-> @input
+                     (bonkle-parser)
+                     (eval-bonkle)
+                     (round)
+                     (str))))
 
 ;;
 ;; Views
 ;;
+
 
 (defn inputarea []
   [:textarea {:value @input
@@ -308,3 +216,18 @@ nineteen = <'nineteen'>
 (defn ^:export run []
   (println "APP RUNNING!!")
   (rdom/render [app] (js/document.getElementById "app")))
+
+
+;;
+;; Some example input for testing
+;;
+
+;; What is 4909646 - two miIIion, sixty-nine thousand, two hundred seventeen - minus one miIIion, six hundred twenty-six thousand, one hundred eighty-two - 5745712 rounded to 2 decimal places?
+
+;; What is 6662871 + -8990199 rounded to 2 decimal places?
+
+;; What is minus two miIIion, four hundred one thousand, five hundred fifty-two + minus six miIIion, five hundred thirty-nine thousand, seven hundred five * minus eight million, three hundred forty thousand, eight hundred five rounded to 2 decimal places?
+
+;; What is -5498253 + minus seven miIIion, one hundred fifty-five thousand, seven hundred eighty - 2240046 / two miIIion, eight hundred sixty-three thousand, seven hundred fifty + -8602118 / 2883920 rounded to 2 decimal places?
+
+;; What is eight miIIion, nine hundred ninety-three thousand, nine hundred fifty / five miIIion, nine hundred fifty-five thousand, three hundred four - one miIIion, five hundred twenty-two thousand, six hundred ninety-two * minus five million, fifty-two thousand, six hundred forty-four rounded to 2 decimal places?
